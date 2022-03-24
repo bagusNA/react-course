bd07588dd64d0e4765229039e95b0d346dc45572
Bagus Nur A <bagusnur@protonmail.com>
2022-03-24 14:54:35 +0800

    uhhuh

--- tip-calculator-2/src/App/App.jsx
+++ tip-calculator-2/src/App/App.jsx
@@ -1,7 +1,37 @@
+import { ReactComponent as Logo} from '../images/logo.svg';
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="min-h-screen flex flex-col bg-secondary-light-cyan-1 font-['Space_Mono']">
+      <div className="flex justify-center items-center py-12">
+        <Logo />
+      </div>
+      <div className="bg-white flex-1 flex flex-col rounded-t-3xl p-8">
+        {/* Inputs */}
+        <div className="flex-1">
+          <div className="flex flex-col">
+            <label htmlFor="bill">Bill</label>
+            <div className="flex py-3 bg-secondary-light-cyan-2 rounded-md">
+              Icon
+              <input type="text" id="bill" name="bill"
+                className="flex-1 bg-secondary-light-cyan-2"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Results */}
+        <div className="flex flex-col gap-6 p-5 bg-accent-dark-cyan rounded-2xl">
+          <div className="py-2">
+            <h1>Results</h1>
+            <h1>Results</h1>
+          </div>
+
+          <button className="bg-primary-cyan rounded-lg p-4">
+            RESET
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- tip-calculator-2/tailwind.config.js
+++ tip-calculator-2/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-cyan': '#26C0AB',
+        'secondary-dark-cyan-1': '#5E7A7D',
+        'secondary-dark-cyan-2': '#7F9C9F',
+        'secondary-light-cyan-1': '#C5E4E7',
+        'secondary-light-cyan-2': '#F4FAFA',
+        'accent-dark-cyan': '#00494D',
+      }
+    },
   },
   plugins: [],
 }
