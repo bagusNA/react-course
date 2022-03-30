7b92aa133c8bfaf86a103d21715e294d57d0a83a
Bagus Nur A <bagusnur@protonmail.com>
2022-03-30 10:34:43 +0800

    add rounding in functions

---
+++ tip-calculator-2/src/App/functions/tipPerPerson.js
@@ -0,0 +1,9 @@
+export default function tipPerPerson(bill, tipPercent, nPeople) {
+  bill = parseInt(bill);
+  nPeople = parseInt(nPeople);
+  tipPercent = parseInt(tipPercent);
+
+  let total = (bill * tipPercent / 100) / nPeople;
+
+  return Number.isInteger(total) ? total : total.toFixed(4);
+}
---
+++ tip-calculator-2/src/App/functions/totalPerPerson.js
@@ -0,0 +1,10 @@
+export default function totalPerPerson(bill, tipPercent, nPeople) {
+  bill = parseInt(bill);
+  nPeople = parseInt(nPeople);
+  tipPercent = parseInt(tipPercent);
+
+  let total = ( bill + (bill * tipPercent / 100) ) / nPeople;
+
+  // Round to 4 decimal places if float
+  return Number.isInteger(total) ? total : total.toFixed(4);
+}
